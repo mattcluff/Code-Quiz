27f8035f452dd58557164a237bea46e290e2a0af
Matt Cluff <matthewrcluff@gmail.com>
2019-12-12 23:16:52 -0700

    started JavaScript Q's

---
+++ questions.js
@@ -0,0 +1,27 @@
+const questions = [{
+    titles: "1. Commonly used data types DO NOT include: ",
+    options: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    titles: "2. The condition in an if / else statement is enclosed within ____.",
+    options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    titles: "3. How does one comment out code in JavaScript?",
+    options: ["/* /*", "<!---->", "//", "!//"],
+    answer: "//"
+  },
+  {
+    titles: "4. What is the most specific equality comparison symbol?",
+    options: ["=", "===", "==", "!!="],
+    answer: "==="
+  },
+  {
+    titles: "5. What is the difference between for loop and while loops?",
+    options: ["<javascript>", "<scripting>", "<script>", "<js>"],
+    answer: "<script>"
+  },
+  ];
+  
